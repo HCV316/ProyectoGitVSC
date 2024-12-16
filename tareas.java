085cba1befc7ad3712dc832a09b5869a4d4476f8
HCV316 <hectoriespuzol316@gmail.com>
2024-12-16 22:49:28 +0100

    Añadido tareas.java

---
+++ tareas.java
@@ -0,0 +1,10 @@
+public class tareas{
+    public static void tareas(){
+        System.out.println("Tarea 1: ...");
+        System.out.println("Tarea 2: ...");
+        System.out.println("Tarea 3: ...");
+    }
+    public static void addtareas(){
+        System.out.println("Añade una tarea aqui");
+    }
+}
