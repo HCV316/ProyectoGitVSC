2b9eca341517fc14315745acf45b940b3a914e10
HCV316 <hectoriespuzol316@gmail.com>
2024-12-16 23:01:20 +0100

    Saludo.java creado

---
+++ Saludo.java
@@ -0,0 +1,8 @@
+public class saludo{
+  public static void Saludar{
+    System.out.println("Hola, como estas");
+  }
+  public static void Despedir{
+    System.out.println("Hasta la proxima");
+  }
+}
